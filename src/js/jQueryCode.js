64d2c0d0aa70337e0449cdc7f0ceb286ffb8edde
David Perez <dperezval08@gmail.com>
2020-05-21 23:32:26 -0500

    Tabular estudiante con mejor y peor promedio

--- src/js/jQueryCode.js
+++ src/js/jQueryCode.js
@@ -1,5 +1,7 @@
 $(() => {
   $('input[type="submit"]').hide();
+  let worstAverage = 5;
+  let bestAverage = 0;
 
   // Funcion para validar todo el formulario
   const validateData = () => {
@@ -60,11 +62,63 @@ $(() => {
 
       // Colorear si gana o pierde
       if(studentData[studentData.length - 1] >= 3.5){
-        $("tbody tr:last-child").addClass("gana");
+        $("#CompleteList tr:last-child").addClass("gana");
       } else if(studentData[studentData.length - 1] < 3) {
-        $("tbody tr:last-child").addClass("pierde");
+        $("#CompleteList tr:last-child").addClass("pierde");
       } else {
-        $("tbody tr:last-child").addClass("neutro");
+        $("#CompleteList tr:last-child").addClass("neutro");
+      }
+
+      // Verificar si es el mejor o peor promedio
+      if(studentData[studentData.length - 1] >= bestAverage){
+        bestAverage = studentData[studentData.length - 1];
+        $("#BestAverage").html(`
+          <thead>
+            <tr>
+              <th>Cédula</th>
+              <th>Nombre</th>
+              <th>Nota 1</th>
+              <th>Nota 2</th>
+              <th>Nota 3</th>
+              <th>Promedio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${studentData[0]}</td>
+              <td>${studentData[1]}</td>
+              <td>${studentData[2]}</td>
+              <td>${studentData[3]}</td>
+              <td>${studentData[4]}</td>
+              <td>${studentData[5]}</td>
+            </tr>
+          </tbody>`
+        )
+      }
+      if(studentData[studentData.length - 1] <= worstAverage){
+        worstAverage = studentData[studentData.length - 1];
+        $("#WorstAverage").html(`
+          <thead>
+            <tr>
+              <th>Cédula</th>
+              <th>Nombre</th>
+              <th>Nota 1</th>
+              <th>Nota 2</th>
+              <th>Nota 3</th>
+              <th>Promedio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${studentData[0]}</td>
+              <td>${studentData[1]}</td>
+              <td>${studentData[2]}</td>
+              <td>${studentData[3]}</td>
+              <td>${studentData[4]}</td>
+              <td>${studentData[5]}</td>
+            </tr>
+          </tbody>`
+        )
       }
     }
   }
@@ -80,12 +134,13 @@ $(() => {
     return age;
   }
 
-  // Evento disparado al momento de dar click en el botón
+  // Evento disparado al momento de dar click en el botón de Guardar
   $("form").submit((e) => {
     e.preventDefault();
     validateData();
   })
 
+  // Evento para detectar el cambio de fecha
   $('input[type="date"]').change((e) => {
     const miEdad = calcularEdad(e.target.value);
     if(miEdad >= 18){
@@ -94,4 +149,15 @@ $(() => {
       $('input[type="submit"]').hide(1000);
     }
   })
+
+  // Evento al hacer click en la opción "Estudiantes" de la barra lateral
+  $("#EstudiantesSection").click(() => {
+    alert("Usted ya se encuentra en la sección de Estudiantes")
+  })
+
+  // Evento al hacer click en la opción "Profesores" de la barra lateral
+  $("#ProfesoresSection").click(() => {
+    alert("La sección de Profesores aún no está disponible.")
+  })
+
 })
